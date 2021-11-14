491dcd8ff13cb7d076192c574125afa3ec7733c3
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2021-11-14 16:27:48 +0700

    setup search function

---
+++ controllers/client/index.js
@@ -0,0 +1,21 @@
+
+
+var data = require('../../models/disease_data')
+var CheckLogin = require('../../models/login_checker_forindex')
+exports.index = (req, res) => {
+    var checklogin = CheckLogin(req, res)
+    if (checklogin == false) {
+        res.render('index', { login: false });
+    } else {
+        var id = req.signedCookies.UUID
+        data.findOne({ '_id': id }).exec((err, user) => {
+            var cuser = user.user
+            res.render("index", { login: true, userdata: cuser, rmoney: user.rmoney })
+        })
+    }
+}
+exports.Search = (req, res) => {
+    data.find({ location: req.body.search }).exec((err, user) => {
+        res.send(user)
+    })
+}
